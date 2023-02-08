baba8cc8aa423228e3e60be5a658240b2ccc36dc
FabrizioSG <fabriziosg95@gmail.com>
2023-02-07 18:51:47 -0600

    Casilla Feliz pt1

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -3,63 +3,91 @@ import React, { useState, useEffect } from 'react';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
-import ButtonBase from '@mui/material/ButtonBase';
-import { styled } from '@mui/material/styles';
 import Typography from '@mui/material/Typography';
 import Avatar from '@mui/material/Avatar';
 import HistorialPosts from './historialPost';
 import { useSearchParams } from 'react-router-dom';
 import axios from 'axios';
-import { useNavigate  } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import moment from 'moment';
+import Button from '@mui/material/Button';
+import Autocomplete from '@mui/material/Autocomplete';
+import TextField from '@mui/material/TextField';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
 
-
-const Img = styled('img')({
-    margin: 'auto',
-    display: 'block',
-    maxWidth: '100%',
-    maxHeight: '100%',
-});
 
 export default function MainDashboard() {
     const navigate = useNavigate();
-    const [posts, setPosts] = useState([]);
+    const [anno, setAnno] = useState('');
 
-    useEffect(() => {
-        try {
-            axios
-                .post("http://localhost:3010/posts/cola", {
-                    usuario: localStorage.getItem('id'),
-                })
-                .catch(function (error) {
-                    console.log(error.response.data.data);
-                })
-                .then((response) => {
-                    if (response.data.message === "OK") {
-                        console.log(response.data.data)
-                        setPosts(response.data.data)
-                    }
-                });
-        } catch (err) {
-            alert(err);
-        }
-    }, []);
+    const handleChange = (event) => {
+        setAnno(event.target.anno);
+        console.log(anno)
+    };
+    const monthNames = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Sept", "Octubre", "Noviembre", "Diciembre"];
+
+    /*  useEffect(() => {
+         try {
+             axios
+                 .post("http://localhost:3010/posts/cola", {
+                     usuario: localStorage.getItem('id'),
+                 })
+                 .catch(function (error) {
+                     console.log(error.response.data.data);
+                 })
+                 .then((response) => {
+                     if (response.data.message === "OK") {
+                         console.log(response.data.data)
+                     }
+                 });
+         } catch (err) {
+             alert(err);
+         }
+     }, []); */
 
     return (
-
-        <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
-            <Grid container spacing={3}>
-                <Grid item xs={12} md={8} lg={9}>
-                    <Paper
-                        sx={{
-                            p: 2,
-                            display: 'flex',
-                            flexDirection: 'column',
-                            height: 240,
-                        }}
+        <Container>
+            <Paper >
+                <Button variant="contained" color="success" sx={{ m: 3 }}>
+                    + Salario Neto
+                </Button>
+                <Button variant="contained" color="success" sx={{ m: 3 }}>
+                    + Otros Ingresos
+                </Button>
+                <Button variant="contained" color="success" sx={{ m: 3 }}>
+                    + Ahorros
+                </Button>
+                <Button variant="contained" color="success" sx={{ m: 3 }}>
+                    + Deuda
+                </Button>
+                <Button variant="contained" color="success" sx={{ m: 3 }}>
+                    + Gasto Recurrente
+                </Button>
+                <FormControl sx={{minWidth:120}}>
+                    <InputLabel id="demo-controlled-open-select-label">Año</InputLabel>
+                    <Select
+                        labelId="demo-controlled-open-select-label"
+                        id="demo-controlled-open-select"
+                        value={anno}
+                        onChange={handleChange}
                     >
-                    </Paper>
+                        <MenuItem value={10}>Ten</MenuItem>
+                        <MenuItem value={20}>Twenty</MenuItem>
+                        <MenuItem value={30}>Thirty</MenuItem>
+                    </Select>
+                </FormControl>
+                <Grid container sx={{ mt: 3 }}>
+                    {monthNames.map((monthName) => (
+                        <Grid item xs={1} sx={{ border: 1 }} key={monthName}>
+                            {monthName}
+                        </Grid>
+                        
+                    ))}
                 </Grid>
-            </Grid>
+            </Paper>
         </Container>
 
     );
